14b77203ec70c9e48eedd262e26faee4a26f80ba
laofeiyang0220 <72029829+laofeiyang0220@users.noreply.github.com>
2021-01-11 21:22:03 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function opens(){
+    var x = document.getElementById("homework");
+    var sel = x.selectedIndex;
+    if(sel==1) window.open("a.html","main");
+    if(sel==2) window.open("b.html","main");
+    if(sel==3) window.open("c.html","main");
+    if(sel==4) window.open("d.html","main");
+    if(sel==5) window.open("e.html","main");
+}
